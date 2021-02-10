0cbef2c3d79d20f1ce235b1fc15da525ee08821a
Rosy <rosyranipatel123@gmail.com>
2021-02-10 22:23:23 +0000

    Initial Commit

---
+++ MyDatePickerProgrammaticWay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDatePickerProgrammaticWay
+//
+//  Created by Alok Kumar Naik on 10/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
